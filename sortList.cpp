e6502f174031c4e704e926e06a1a6bd44bed56ec
Kaushik <kaushikmandavkar6@gmail.com>
2024-04-12 21:10:25 +0530

    .

--- sortList.cpp
+++ sortList.cpp
@@ -8,48 +8,56 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* sortList(ListNode* head) {
+    ListNode *sortList(ListNode *head)
+    {
         if (head == nullptr || head->next == nullptr)
             return head;
-        
+
         // Split the list into two halves
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != nullptr && fast->next != nullptr) {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast != nullptr && fast->next != nullptr)
+        {
             slow = slow->next;
             fast = fast->next->next;
         }
-        ListNode* secondHalf = slow->next;
+        ListNode *secondHalf = slow->next;
         slow->next = nullptr;
-        
+
         // Recursively sort each half
-        ListNode* sortedFirstHalf = sortList(head);
-        ListNode* sortedSecondHalf = sortList(secondHalf);
-        
+        ListNode *sortedFirstHalf = sortList(head);
+        ListNode *sortedSecondHalf = sortList(secondHalf);
+
         // Merge the sorted halves
         return merge(sortedFirstHalf, sortedSecondHalf);
     }
-    
+
     // Merge two sorted linked lists
-    ListNode* merge(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* current = dummy;
-        
-        while (list1 && list2) {
-            if (list1->val <= list2->val) {
+    ListNode *merge(ListNode *list1, ListNode *list2)
+    {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *current = dummy;
+
+        while (list1 && list2)
+        {
+            if (list1->val <= list2->val)
+            {
                 current->next = list1;
                 list1 = list1->next;
-            } else {
+            }
+            else
+            {
                 current->next = list2;
                 list2 = list2->next;
             }
             current = current->next;
         }
-        
+
         current->next = list1 ? list1 : list2;
-        
+
         return dummy->next;
     }
 };
