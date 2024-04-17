9c1005df144b6bbf3b0b74adb6de9cd10f3d5a4c
Kaushik <kaushikmandavkar6@gmail.com>
2024-04-17 19:18:56 +0530

    moves all zero at last in array

---
+++ movesZeroAtLast.cpp
@@ -0,0 +1,35 @@
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int nonZeroIndex = 0; 
+
+        for (int i = 0; i < n; ++i) {
+            // If the current element is non-zero
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
+            }
+        }
+
+        // Fill the remaining positions with zeros
+        for (int i = nonZeroIndex; i < n; ++i) {
+            nums[i] = 0;
+        }
+    }
+};
