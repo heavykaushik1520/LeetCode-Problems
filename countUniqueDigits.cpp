5fc155b946918fdd4e35253916bfb26b4568547f
Kaushik <kaushikmandavkar6@gmail.com>
2024-04-18 18:23:56 +0530

    count unique numbers in digits

---
+++ countUniqueDigits.cpp
@@ -0,0 +1,39 @@
+/*
+Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10n.
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 91
+Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, excluding 11,22,33,44,55,66,77,88,99
+Example 2:
+
+Input: n = 0
+Output: 1*/
+
+#include <vector>
+
+class Solution
+{
+public:
+    int countNumbersWithUniqueDigits(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        int result = 10;
+        int uniqueDigits = 9;
+        int availableNumbers = 9;
+
+        for (int i = 2; i <= n && availableNumbers > 0; ++i)
+        {
+            uniqueDigits *= availableNumbers;
+            result += uniqueDigits;
+            availableNumbers--;
+        }
+
+        return result;
+    }
+};
